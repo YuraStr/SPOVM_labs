2967aa43f10af95f358ff1d6d74f40467bb2e38f
Yury Strukov <strukov.yura.1996@gmail.com>
2016-04-19 07:23:30 +0300

    lab3 linux & windows
    Change-Id: I30c8d6e751ac9f97a565b2e062473417b40e5000

---
+++ lab3/client.cpp
@@ -0,0 +1,71 @@
+#ifdef __linux__
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define NAMEDPIPE_NAME "/tmp/my_named_pipe"
+#define BUFSIZE 50
+
+int main (int argc, char ** argv) {
+  int fd;
+  char buf[BUFSIZE];
+
+  if ( (fd = open(NAMEDPIPE_NAME, O_WRONLY)) <= 0 ) {
+    perror("open");
+    return 1;
+  }
+
+  printf("Client: start\n");
+  while(1) {
+    memset(buf, '\0', BUFSIZE);
+    printf ("Enter message: ");
+    scanf("%s", buf);
+    write(fd, buf, strlen(buf));
+    if (!strcmp(buf, "exit")) break;
+  }
+
+  close(fd);
+
+  return 0;
+}
+#elif _WIN32
+#include <stdio.h>
+#include <windows.h>
+
+#define NAMEDPIPE_NAME "\\\\.\\pipe\\Pipe"
+#define BUFSIZE 80
+
+int main(void)
+{
+  HANDLE hPipe;
+  DWORD dwWritten;
+  char buf[80];
+
+  hPipe = CreateFile(TEXT(NAMEDPIPE_NAME), 
+                     GENERIC_READ | GENERIC_WRITE, 
+                     0,
+                     NULL,
+                     OPEN_EXISTING,
+                     0,
+                     NULL);
+  if (hPipe != INVALID_HANDLE_VALUE) {
+    printf("Client: start\n");
+    while (1) {
+      printf("Enter message: ");
+      scanf("%s", buf);
+      WriteFile(hPipe,
+                buf,
+                strlen(buf),   // = length of string + terminating '\0' !!!
+                &dwWritten,
+                NULL);
+      if (!strcmp(buf, "exit")) break;
+    }
+    CloseHandle(hPipe);
+  }
+
+  return (0);
+}
+#endif
---
+++ lab3/server.cpp
@@ -0,0 +1,78 @@
+#ifdef __linux__
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define NAMEDPIPE_NAME "/tmp/my_named_pipe"
+#define BUFSIZE 50
+
+int main (int argc, char ** argv) {
+  pid_t pid;
+  int fd, len;
+  char buf[BUFSIZE];
+
+  if (mkfifo(NAMEDPIPE_NAME, 0777)) {
+    perror("mkfifo");
+    return 1;
+  }
+  printf("Server: start\n");
+
+  if ((fd = open(NAMEDPIPE_NAME, O_RDONLY)) <= 0 ) {
+    perror("open");
+    return 1;
+  }
+
+  while(1) {
+    memset(buf, '\0', BUFSIZE);
+    len = read(fd, buf, BUFSIZE-1);
+    if (!strcmp(buf, "exit")) break;
+    printf("Recieved: %s\n", buf);
+  }
+  
+  close(fd);
+  remove(NAMEDPIPE_NAME);
+
+  return 0;
+}
+
+#elif _WIN32
+#include <Windows.h>
+#include <stdio.h>
+
+#define NAMEDPIPE_NAME "\\\\.\\pipe\\Pipe"
+#define BUFSIZE 80
+
+int main()
+{
+  HANDLE hPipe;
+  char buffer[BUFSIZE];
+  DWORD dwRead;
+
+  hPipe = CreateNamedPipe(TEXT(NAMEDPIPE_NAME),
+                          PIPE_ACCESS_DUPLEX | PIPE_TYPE_BYTE | PIPE_READMODE_BYTE,
+                          PIPE_WAIT,
+                          1,
+                          1024 * 16,
+                          1024 * 16,
+                          NMPWAIT_USE_DEFAULT_WAIT,
+                          NULL);
+
+  printf("Server: start\n");
+  if (ConnectNamedPipe(hPipe, NULL) != FALSE) {  // wait for someone to connect to the pipe
+	printf("New pipe was connected\n");
+    while (ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE) {
+      buffer[dwRead] = '\0';
+      printf("Recieved: %s\n", buffer);
+      if (!strcmp(buffer, "exit")) break;
+    }
+  }
+  DisconnectNamedPipe(hPipe);
+
+  CloseHandle(hPipe);
+
+  return 0;
+}
+#endif
